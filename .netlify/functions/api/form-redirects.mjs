511e681f87530a55f94a8edd62d1261c92eb7825
Eduardo Roth <eduardo@eduardoroth.dev>
2024-05-16 16:20:16 -0600

    feat(): add netlify function

---
+++ .netlify/functions/api/form-redirects.mjs
@@ -0,0 +1,33 @@
+export default async (req, context) => {
+  try {
+    const WEBFLOW_CRM_TOKEN = Netlify.env.get("WEBFLOW_CRM_TOKEN");
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        authorization: `Bearer ${WEBFLOW_CRM_TOKEN}`,
+      },
+    };
+
+    const rules = await fetch(
+      "https://api.webflow.com/v2/collections/664621e4859f07541bec2c08/items",
+      options,
+    ).then((response) => response.json());
+
+    return new Response(
+      rules.items.map((item) => ({
+        ...item.fieldData,
+        urlstring:
+          item.fieldData.urlstring === "ef22e62c3fb0576d85af86dc6a815e3f"
+            ? "contains"
+            : "not-contains",
+      })),
+    );
+  } catch (err) {
+    return new Response(err, { status: 500 });
+  }
+};
+
+export const config = {
+  path: "/form-redirects",
+};
